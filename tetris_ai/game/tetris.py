fbfdc035eacef227261eacb92db02a505d90dc77
akriel <akrielz@yahoo.com>
2024-01-07 20:20:07 +0200

    finished core game

--- tetris_ai/game/tetris.py
+++ tetris_ai/game/tetris.py
@@ -14,19 +14,23 @@ class TetrisEnv:
             width: int = 10,
             height: int = 22,
             clock_speed: int = 0,  # in milliseconds
+            num_next_shapes: int = 3,
     ):
         # Save clock speed
         self.clock_speed = clock_speed
+        self.num_next_shapes = num_next_shapes
 
         # Create the displayable parts
         self.held_shape_display = np.zeros((2, 4))
         self.board = Board(width, height)
+        self.next_shape_display = np.zeros(((2 + 1) * num_next_shapes, 4))
 
         # Compute offsets for display
-        self.held_shape_offset = np.array([0, 0])
+        self.held_shape_offset = np.array([1, 0])
         self.board_offset = np.array([0, 5])  # len of shape + 1
+        self.next_shape_offset = np.array([1, self.board_offset[1] + width + 1])
 
-        self.display_width = self.held_shape_display.shape[1] + 1 + width
+        self.display_width = self.held_shape_display.shape[1] + 1 + width + 1 + self.next_shape_display.shape[1]
 
         self.display_matrix = np.zeros((height, self.display_width))
 
@@ -35,6 +39,7 @@ class TetrisEnv:
         self.start_position = np.array([0, width // 2])
 
         self.current_shape = self.generate_random_shape()
+        self.next_shapes = [self.generate_random_shape() for _ in range(num_next_shapes)]
         self.hold_shape = None
 
     def add_to_display_positions(self, positions: ListVector2, offset: Vector2):
@@ -45,7 +50,9 @@ class TetrisEnv:
 
         # Display the board
         board_state = self.board.state
-        self.display_matrix[:, self.board_offset[1]:] = board_state
+        start_board_x = self.board_offset[1]
+        stop_board_x = board_state.shape[1] + self.board_offset[1]
+        self.display_matrix[:, start_board_x: stop_board_x] = board_state
 
         # Display the held shape
         if self.hold_shape is not None:
@@ -54,10 +61,29 @@ class TetrisEnv:
         # Display the current shape
         self.add_to_display_positions(self.current_shape.blocks_position, self.board_offset)
 
+        # Display the next shapes
+        for i, shape in enumerate(self.next_shapes):
+            self.add_to_display_positions(shape.shape_id.blocks_position, self.next_shape_offset + np.array([i * (2 + 1), 0]))
+
     def generate_random_shape(self):
         return self.shape_generator.generate_random_shape(self.start_position)
 
-    def step(self, action: Action):
+    def clear_lines(self):
+        ys = self.current_shape.blocks_position[:, 0]
+        for y in ys:
+            if np.all(self.board[y, :] == 1):
+                self.board[y, :] = 0
+                self.board[1:y+1, :] = self.board[:y, :]
+                self.board[0, :] = 0
+
+    def place_shape(self):
+        self.current_shape.place()
+        self.clear_lines()
+
+        self.current_shape = self.next_shapes.pop(0)
+        self.next_shapes.append(self.generate_random_shape())
+
+    def process_action(self, action: Action):
         if action == Action.LEFT:
             self.current_shape.move(np.array([0, -1]))
 
@@ -68,8 +94,7 @@ class TetrisEnv:
             if self.current_shape.can_move_down():
                 self.current_shape.move(np.array([1, 0]))
             else:
-                self.current_shape.place()
-                self.current_shape = self.generate_random_shape()
+                self.place_shape()
 
         elif action == Action.ROTATE:
             self.current_shape.rotate(is_clockwise=True)
@@ -77,8 +102,7 @@ class TetrisEnv:
         elif action == Action.DROP:
             while self.current_shape.can_move_down():
                 self.current_shape.move(np.array([1, 0]))
-            self.current_shape.place()
-            self.current_shape = self.generate_random_shape()
+            self.place_shape()
 
         elif action == Action.NOOP:
             pass
@@ -93,6 +117,8 @@ class TetrisEnv:
                 self.hold_shape.reset()
                 self.current_shape, self.hold_shape = self.hold_shape, self.current_shape
 
+    def step(self, action: Action):
+        self.process_action(action)
         self.update_display_matrix()
 
         state = self.display_matrix
